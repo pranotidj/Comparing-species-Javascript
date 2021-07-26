6de520825365f431756f609b478b6600f3e178bc
Pranoti Jambhale <pranotidj@gmail.com>
2021-07-26 16:59:40 -0700

    Added comments, deleted console lines

--- app.js
+++ app.js
@@ -36,9 +36,10 @@ function getAllFormData(){
         .addEventListener('click',function (event) {
                 
             let inputs = document.getElementById('dino-compare').elements;
-            let flag = validateFormData(inputs);
+            let flag = validateFormData(inputs);  //check if user entered data correctly
             if(!flag)
                 event.preventDefault();
+
             human['name'] = inputs['name'].value;
             let enteredFeet = parseInt(inputs['feet'].value);
             let enteredInches = parseInt(inputs['inches'].value);
@@ -50,7 +51,7 @@ function getAllFormData(){
             if(Object.keys(human).length === 0){
                 return;
             }
-            compareHumanWithDinos(jsonData, human);
+            compareHumanWithDinos(jsonData, human); 
 
         });
     
@@ -82,7 +83,6 @@ function validateFormData(inputs){
     }
     
     if(!flag){
-        console.log(errorMessage);
         document.getElementById("err").innerHTML = "Enter your" + errorMessage;
         return;
     }
@@ -97,8 +97,7 @@ function isEmpty(str) {
 
    
 function compareHumanWithDinos(dino, human){
-    console.log("in compare");
-    let dinoArray = [];
+   let dinoArray = [];
     for(i = 0; i < jsonData.length; i++){
         dObject = new Dino(jsonData[i]);
         dSpecies = dObject.species;
@@ -107,9 +106,11 @@ function compareHumanWithDinos(dino, human){
         dDiet = dObject.diet;
         dFacts = dObject.factArray;
         
+        //call to functions to compare data
         let fact1 = compareHeight(dSpecies, dHeight, human.height);
         let fact2 = compareWeight(dSpecies, dWeight, human.weight);
         let fact3 = compareDiet(dSpecies, dDiet, human.diet);
+        
         if(!isEmpty(fact1)){
             dFacts.push(fact1);     //adding facts to Dino objects
         }
@@ -126,97 +127,92 @@ function compareHumanWithDinos(dino, human){
     generateTiles(dinoArray, human);
     
 }
-    // Create Dino Compare Method 1
-    // NOTE: Weight in JSON file is in lbs, height in inches. 
-    
-    function compareHeight(dSpecies, dHeight, hHeight){
-        let fact = "";
-        if(dHeight < hHeight){
-            let diff = Math.abs(hHeight - dHeight);
-            fact = `You are taller than ${dSpecies} by ${diff} inches`;
-        }else
-            fact = `${dSpecies} is taller than you`;
-         return fact;   
-    }
-    
-    // Create Dino Compare Method 2
-    // NOTE: Weight in JSON file is in lbs, height in inches.
-    function compareWeight(dSpecies, dWeight, hWeight){
-        let fact = "";
-        if(dWeight < hWeight){
-            let diff = Math.abs(hWeight - dWeight);
-            fact = `Surprisingly, you are heavier than ${dSpecies} by ${diff} lbs`;
-        }else
-            fact = `${dSpecies} heavier than you`;
+// Create Dino Compare Method 1
+// NOTE: Weight in JSON file is in lbs, height in inches. 
+
+function compareHeight(dSpecies, dHeight, hHeight){
+    let fact = "";
+    if(dHeight < hHeight){
+        let diff = Math.abs(hHeight - dHeight);
+        fact = `You are taller than ${dSpecies} by ${diff} inches`;
+    }else
+        fact = `${dSpecies} is taller than you`;
         return fact;   
-    }
+}
     
-    // Create Dino Compare Method 3
-    // NOTE: Weight in JSON file is in lbs, height in inches.
-    function compareDiet(dSpecies, dDiet, hDiet){
-        let fact = "";
-        if(dDiet === hDiet){
-            fact = `${dSpecies} were ${dDiet}, just like you`;
-        }else
-            fact = `Unlike you, ${dSpecies} were ${dDiet}`;
-        return fact;   
-    }
+// Create Dino Compare Method 2
+// NOTE: Weight in JSON file is in lbs, height in inches.
+function compareWeight(dSpecies, dWeight, hWeight){
+    let fact = "";
+    if(dWeight < hWeight){
+        let diff = Math.abs(hWeight - dWeight);
+        fact = `Surprisingly, you are heavier than ${dSpecies} by ${diff} lbs`;
+    }else
+        fact = `${dSpecies} heavier than you`;
+    return fact;   
+}
     
-    // Generate Tiles for each Dino in Array
-    function generateDinoArray(dObject){
-        const dinoArray = [];
-        let speciesName = dObject.species;
-        dinoArray.push(speciesName);
-        let randomFact = dObject.factArray[Math.floor(Math.random() * dObject.factArray.length)];
-        if(dObject.species === "Pigeon") {
-            randomFact = "All birds are living dinosaurs";
-        }
-        dinoArray.push(randomFact);
-        let speciesImage = encodeURI(speciesName.toLowerCase());
-        let imageName =  speciesImage + '.png';
-        dinoArray.push(imageName);
-        return dinoArray;
+// Create Dino Compare Method 3
+// NOTE: Weight in JSON file is in lbs, height in inches.
+function compareDiet(dSpecies, dDiet, hDiet){
+    let fact = "";
+    if(dDiet === hDiet){
+        fact = `${dSpecies} were ${dDiet}, just like you`;
+    }else
+        fact = `Unlike you, ${dSpecies} were ${dDiet}`;
+    return fact;   
+}
+
+// Generate Tiles for each Dino in Array
+function generateDinoArray(dObject){
+    const dinoArray = [];
+    let speciesName = dObject.species;
+    dinoArray.push(speciesName);
+    let randomFact = dObject.factArray[Math.floor(Math.random() * dObject.factArray.length)];
+    if(dObject.species === "Pigeon") {
+        randomFact = "All birds are living dinosaurs";
     }
-   function generateTiles(dinoArray, human){
-        if(isEmpty(human.name)){
-            return;
-        }
-        let speciesArray = [...dinoArray];
-        let humanData = [human.name, '', 'human.png'];
-        speciesArray.splice(4, 0, humanData);   // added human at the center of an matrix
-        console.log(speciesArray);
-        console.log("i m here");
-        console.log(speciesArray.length);
-        if(speciesArray.length === 9)
-            addTilesToDom(speciesArray);
-   }
+    dinoArray.push(randomFact);
+    let speciesImage = encodeURI(speciesName.toLowerCase());
+    let imageName =  speciesImage + '.png';
+    dinoArray.push(imageName);
+    return dinoArray;
+}
+function generateTiles(dinoArray, human){
+    if(isEmpty(human.name)){
+        return;
+    }
+    let speciesArray = [...dinoArray];
+    let humanData = [human.name, '', 'human.png'];
+    speciesArray.splice(4, 0, humanData);   // added human at the center of an matrix
+    if(speciesArray.length === 9)
+        addTilesToDom(speciesArray);
+}
     
+ // On button click, prepare and display infographic  
+ // Remove form from screen
+// Add tiles to DOM
+function addTilesToDom(speciesArray){
+    let tiles = '';
+
+    speciesArray.forEach(function traverseThruArray(element){
+        let tileClassName = "grid-item";
+        let imageName = element[2];
+        let imgPath = `images/${imageName}`;
+        tiles =  `
+                <div class=${tileClassName}>
+                    <h3>${element[0]}</h3>
+                    <img src=${imgPath}>
+                    <p>${element[1]}</p>
+                </div>`
+        document.getElementById("grid").innerHTML += tiles;
+    });
+    if(!isEmpty(tiles))
+            document.getElementById('dino-compare').style.display = "none";
+
+}
+
    
 
-     // Add tiles to DOM
-    function addTilesToDom(speciesArray){
-        console.log("generate tiles");
-        let tiles = '';
-    
-        speciesArray.forEach(function traverseThruArray(element){
-            let tileClassName = "grid-item";
-            let imageName = element[2];
-            let imgPath = `images/${imageName}`;
-            console.log(imgPath);
-            tiles =  `
-                    <div class=${tileClassName}>
-                        <h3>${element[0]}</h3>
-                        <img src=${imgPath}>
-                        <p>${element[1]}</p>
-                    </div>`
-            document.getElementById("grid").innerHTML += tiles;
-        });
-        if(!isEmpty(tiles))
-             document.getElementById('dino-compare').style.display = "none";
-    
-    }
-
-    // Remove form from screen
 
 
-// On button click, prepare and display infographic
